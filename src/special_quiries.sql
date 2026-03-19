1e6710a2c2b62431280ac6a5326f2fd98f4ab903
Hayden J <spikeyslad21@gmail.com>
2026-03-18 20:46:58 -0600

    Added reccomemnded accounts

--- src/special_quiries.sql
+++ src/special_quiries.sql
@@ -9,6 +9,17 @@ AND p.account_id NOT IN (SELECT to_id FROM follows WHERE from_id = ?)
 GROUP BY p.post_id
 ORDER BY likes desc;
 
+-- reccomended accounts, all '?' are the considered account id
+SELECT p.account_id AS reccomended_accounts, count() AS likes 
+FROM follows AS f 
+JOIN likes AS l ON f.to_id = l.account_id 
+JOIN posts AS p ON l.post_id = p.post_id 
+WHERE f.from_id = ? 
+AND p.account_id != ? 
+AND p.account_id NOT IN (SELECT to_id FROM follows WHERE from_id = ?) 
+GROUP BY p.account_id
+ORDER BY likes desc;
+
 -- most popular follows, '?' is the considered account id
 SELECT f.to_id AS followed_account, (count(DISTINCT l.like_id)*1.0)/(count(DISTINCT p.post_id)*1.0) AS ratio 
 FROM follows AS f 
