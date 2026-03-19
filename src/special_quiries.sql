f4b5f345e1259a3f9e5bd4a07d425a4a20c22d6e
Hayden J <spikeyslad21@gmail.com>
2026-03-18 20:36:50 -0600

    Added other query

--- src/special_quiries.sql
+++ src/special_quiries.sql
@@ -1,16 +1,19 @@
--- select username, (count(distinct l.like_id)*1.0 )/(count(distinct p.post_id)*1.0) as ratio 
--- from accounts as a join posts as p on a.account_id = p.account_id 
--- join likes as l on p.post_id = l.post_id group by a.account_id;
-
 -- reccomended posts, all '?' are the considered account id
 SELECT p.post_id AS reccomended_post_id, count() AS likes 
 FROM follows AS f 
 JOIN likes AS l ON f.to_id = l.account_id 
-JOIN posts as p on l.post_id = p.post_id 
+JOIN posts AS p ON l.post_id = p.post_id 
 WHERE f.from_id = ? 
 AND p.account_id != ? 
 AND p.account_id NOT IN (SELECT to_id FROM follows WHERE from_id = ?) 
 GROUP BY p.post_id
 ORDER BY likes desc;
 
--- best follow (most interacted follow), all '?' are the considered account id
+-- most popular follows, '?' is the considered account id
+SELECT f.to_id AS followed_account, (count(DISTINCT l.like_id)*1.0)/(count(DISTINCT p.post_id)*1.0) AS ratio 
+FROM follows AS f 
+JOIN posts AS p on f.to_id = p.account_id 
+LEFT JOIN likes AS l on p.post_id = l.post_id 
+WHERE from_id = ? 
+GROUP BY f.to_idJOIN 
+ORDER BY ratio desc;
